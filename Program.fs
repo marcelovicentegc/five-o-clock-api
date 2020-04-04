65e4a05484e009f2c5ee118865caece26473733e
Marcelo Cardoso <marcelovicentegc@gmail.com>
2020-04-04 15:57:28 -0300

    feat: get closest timezone

--- Program.fs
+++ Program.fs
@@ -1,17 +1,59 @@
-﻿open Suave
+﻿open System
+open Suave
 open Suave.Filters
 open Suave.Operators
 open Suave.Successful
+open Suave.Writers
+open Newtonsoft.Json
 
-let app =
-    choose
-        [ GET >=> choose
-            [ path "/" >=> OK "Index"
-              path "/hello" >=> OK "Hello!" ]
-          POST >=> choose
-            [ path "/hello" >=> OK "Hello POST!" ] ]
+type TZInfo = {
+    zoneName: string; 
+    minDiff: float; 
+    localTime: string; 
+    utcOffset: float
+}
+
+let getClosest () =
+    let timezones = TimeZoneInfo.GetSystemTimeZones()
+
+    let formattedTimezones = [
+      for timezone in timezones do
+        let localTimezone = TimeZoneInfo.ConvertTime(DateTime.Now, timezone)
+        let fiveOClockPM = DateTime(localTimezone.Year, localTimezone.Month, localTimezone.Day, 17, 0, 0)   
+        let minDifference = (localTimezone - fiveOClockPM).TotalMinutes
+
+        yield {
+            zoneName = timezone.StandardName
+            minDiff = minDifference
+            localTime = localTimezone.ToString("hh:mm tt")
+            utcOffset = timezone.BaseUtcOffset.TotalHours
+        }
+    ]
+
+    formattedTimezones
+        |> List.filter (fun (i: TZInfo) -> i.minDiff >= 0.0)
+        |> List.minBy (fun (i:TZInfo) -> i.minDiff)
+
+
+let startServer argv = 
+    let port = 8080
+
+    let cfg =
+              { defaultConfig with
+                  bindings = [ HttpBinding.createSimple HTTP "0.0.0.0" port]}
+
+    let app =
+        choose
+            [ GET >=> choose
+                [ 
+                    path "/" >=> request (fun _ -> OK <| JsonConvert.SerializeObject(getClosest()))
+                        >=> setMimeType "application/json; charset=utf-8"
+                ]
+            ]
+
+    startWebServer cfg app
 
 [<EntryPoint>]
 let main argv =
-    startWebServer defaultConfig app
+    startServer argv
     0
---
+++ README.md
@@ -0,0 +1,7 @@
+# five-o-clock 🕥
+
+## Development ⌨️
+
+1. Clone this repo: `git clone git@github.com:marcelovicentegc/five-o-clock-api.git`
+2. Install dependencies: `dotnet restore`
+3. Start the server: `dotnet run`
